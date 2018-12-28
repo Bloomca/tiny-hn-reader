ee5c2318676b391f966ef46741ca17ffdac8f898
Seva Zaikov <seva.zaikov@gmail.com>
2018-12-27 21:44:00 -0800

    add api code to fetch stories from HN

---
+++ src/api.js
@@ -0,0 +1,21 @@
+const DOMAIN = "https://hacker-news.firebaseio.com/v0";
+
+// request for latest stories
+// https://hacker-news.firebaseio.com/v0/newstories.json
+export function getStories() {
+  return get("/newstories.json");
+}
+
+// example of a single story request
+// https://hacker-news.firebaseio.com/v0/item/8863.json
+export function getStory(id) {
+  return get(`/item/${id}.json`);
+}
+
+// we don't need to support POST requests, so only GET is implemented
+// same goes for query parameters – we don't need them, so they are not
+// implemented.
+// in fact, we don't even expose this function, all API is within this file
+function get(url) {
+  return fetch(`${DOMAIN}${url}`).then(response => response.json());
+}
